52be16a8f1df7671319729dc0847360f3e55aa21
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-11-30 13:04:57 +0100

    get rid of internal representation

--- test/rays/tuples_test.clj
+++ test/rays/tuples_test.clj
@@ -5,12 +5,12 @@
 
 (deftest test-tuples
   (testing "A tuple with w=1.0 is a point"
-    (let [a {:x 4.3 :y -4.2 :z 3.1 :w 1.0}]
+    (let [a (sut/->tuple 4.3 -4.2 3.1 1.0)]
       (is (sut/point? a))
       (is (not (sut/vector? a)))))
 
   (testing "A tuple with w=0 is a vector"
-    (let [a {:x 4.3 :y -4.2 :z 3.1 :w 0}]
+    (let [a (sut/->tuple 4.3 -4.2 3.1 0)]
       (is (not (sut/point? a)))
       (is (sut/vector? a))))
 
