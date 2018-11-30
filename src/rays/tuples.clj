b28f41036a8d6cd14080ab81212ba84d30993f0a
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-11-30 12:49:08 +0100

    compare floats with epsilon

--- src/rays/tuples.clj
+++ src/rays/tuples.clj
@@ -1,4 +1,10 @@
-(ns rays.tuples)
+(ns rays.tuples
+  (:import [java.lang.Math]))
+
+(def epsilon 0.00001)
+
+(defn- eq-floats? [a b]
+  (< (Math/abs (- a b)) epsilon))
 
 (defn ->tuple [x y z w]
   {:x x :y y :z z :w w})
@@ -10,7 +16,7 @@
   (->tuple x y z 0))
 
 (defn point? [t]
-  (= 1.0 (:w t)))
+  (eq-floats? 1.0 (:w t)))
 
 (defn vector? [t]
-  (zero? (:w t)))
+  (eq-floats? 0.0 (:w t)))
