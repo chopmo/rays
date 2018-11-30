43863e6750d739e522e0e6999748421950ebae8b
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-11-30 12:47:05 +0100

    Add functions to create vectors and points

--- src/rays/tuples.clj
+++ src/rays/tuples.clj
@@ -1,5 +1,14 @@
 (ns rays.tuples)
 
+(defn ->tuple [x y z w]
+  {:x x :y y :z z :w w})
+
+(defn ->point [x y z]
+  (->tuple x y z 1))
+
+(defn ->vector [x y z]
+  (->tuple x y z 0))
+
 (defn point? [t]
   (= 1.0 (:w t)))
 
--- test/rays/tuples_test.clj
+++ test/rays/tuples_test.clj
@@ -12,4 +12,16 @@
   (testing "A tuple with w=0 is a vector"
     (let [a {:x 4.3 :y -4.2 :z 3.1 :w 0}]
       (is (not (sut/point? a)))
-      (is (sut/vector? a)))))
+      (is (sut/vector? a))))
+
+  (testing "->tuple creates a tuple"
+    (is (= {:x 1 :y 2 :z 3 :w 0}
+           (sut/->tuple 1 2 3 0))))
+
+  (testing "->point creates tuples with w=1"
+    (is (= (sut/->tuple 4 -4 3 1)
+           (sut/->point 4 -4 3))))
+
+  (testing "->vector creates tuples with w=0"
+    (is (= (sut/->tuple 4 -4 3 0)
+           (sut/->vector 4 -4 3)))))
