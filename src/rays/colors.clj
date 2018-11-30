b5a8de749a793f22c8631dc16e9ddc92acdab5aa
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-11-30 19:29:18 +0100

    color operations

--- src/rays/colors.clj
+++ src/rays/colors.clj
@@ -1,6 +1,27 @@
-(ns rays.colors)
+(ns rays.colors
+  (:require [rays.common :as c]))
 
 (defn ->color [r g b]
   {:red r
    :green g
    :blue b})
+
+(defn equal? [c1 c2]
+  (and (c/eq-floats? (:red c1) (:red c2))
+       (c/eq-floats? (:green c1) (:green c2))
+       (c/eq-floats? (:blue c1) (:blue c2))))
+
+(defn add [c1 c2]
+  (->color (+ (:red c1) (:red c2))
+           (+ (:green c1) (:green c2))
+           (+ (:blue c1) (:blue c2))))
+
+(defn subtract [c1 c2]
+  (->color (- (:red c1) (:red c2))
+           (- (:green c1) (:green c2))
+           (- (:blue c1) (:blue c2))))
+
+(defn multiply [c1 c2]
+  (->color (* (:red c1) (:red c2))
+           (* (:green c1) (:green c2))
+           (* (:blue c1) (:blue c2))))
--- test/rays/colors_test.clj
+++ test/rays/colors_test.clj
@@ -8,4 +8,22 @@
     (let [c (sut/->color 0.1 0.2 0.3)]
       (is (c/eq-floats? 0.1 (:red c)))
       (is (c/eq-floats? 0.2 (:green c)))
-      (is (c/eq-floats? 0.3 (:blue c))))))
+      (is (c/eq-floats? 0.3 (:blue c)))))
+
+  (testing "adding colors"
+    (let [c1 (sut/->color 0.9 0.6 0.75)
+          c2 (sut/->color 0.7 0.1 0.25)]
+      (is (sut/equal? (sut/->color 1.6 0.7 1.0)
+                      (sut/add c1 c2)))))
+
+  (testing "subtracting colors"
+    (let [c1 (sut/->color 0.9 0.6 0.75)
+          c2 (sut/->color 0.7 0.1 0.25)]
+      (is (sut/equal? (sut/->color 0.2 0.5 0.5)
+                      (sut/subtract c1 c2)))))
+
+  (testing "multiplying colors"
+    (let [c1 (sut/->color 1 0.2 0.4 )
+          c2 (sut/->color 0.9 1 0.1)]
+      (is (sut/equal? (sut/->color 0.9 0.2 0.04)
+                      (sut/multiply c1 c2))))))
