0931534f7315feb688d36767ba231dcd931176dd
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-12-01 17:37:26 +0100

    last canvas functions for now

--- src/rays/canvas.clj
+++ src/rays/canvas.clj
@@ -11,3 +11,9 @@
 
 (defn height [c]
   (count c))
+
+(defn write-pixel [c x y col]
+  (assoc-in c [y x] col))
+
+(defn pixel-at [c x y]
+  (get-in c [y x]))
--- test/rays/canvas_test.clj
+++ test/rays/canvas_test.clj
@@ -8,4 +8,11 @@
     (let [c (sut/->canvas 10 20)]
       (is (= 10 (sut/width c)))
       (is (= 20 (sut/height c)))
-      (is (col/equal? col/black (ffirst c))))))
+      (is (col/equal? col/black (ffirst c)))))
+
+  (testing "writing pixesls to a canvas"
+    (let [c (sut/->canvas 10 20)
+          red (col/->color 1 0 0)
+          c (sut/write-pixel c 2 3 red)]
+      (is (col/equal? (col/->color 1 0 0)
+                      (sut/pixel-at c 2 3))))))
