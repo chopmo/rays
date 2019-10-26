47cc661a66bf4ea2c6a340bf350aca7454206517
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-10-26 19:16:59 +0200

    a bit faster shadow rendering
    Because there is no need to replace the black pixels.

--- src/rays/shadow.clj
+++ src/rays/shadow.clj
@@ -8,13 +8,14 @@
 
 (let [canvas-size 100
       canvas      (c/->canvas canvas-size
-                              canvas-size)
+                              canvas-size
+                              (col/->color 0 0 0))
       ray-origin  (t/->point 0 0 -5)
       wall-z      10
       wall-size   7.0
 
       s          (s/sphere)
-      step       3
+      step       2
 
       pixel->coord (fn [x]
                      (* (- x (/ canvas-size 2))
@@ -24,12 +25,10 @@
                   (let [wall-vector (t/->vect (pixel->coord x)
                                               (pixel->coord y)
                                               wall-z)
-                        r           (r/ray ray-origin wall-vector)
-                        xs          (s/intersect s r)
-                        color       (if (seq xs)
-                                      (col/->color 1 0 0)
-                                      (col/->color 0 0 0))]
-                    (c/write-pixel canvas x y color)))]
+                        r           (r/ray ray-origin wall-vector)]
+                    (if (seq (s/intersect s r))
+                      (c/write-pixel canvas x y (col/->color 1 0 0))
+                      canvas)))]
   (->>
    (reduce add-point
            canvas
